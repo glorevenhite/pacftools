ebc69abf88feb2be25646019c46599461946b7e4
Vinh Vo <vinh.votruong@gmail.com>
2013-07-02 20:55:07 +0700

    header template

--- application/views/includes/header.php
+++ application/views/includes/header.php
@@ -2,9 +2,8 @@
 
 <html lang="en">
 <head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"> 
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 	<title>Sign Up!</title>
 	<link rel="stylesheet" href="<?php echo base_url();?>/css/style.css" type="text/css" media="screen" />
-	
 </head>
 <body>
