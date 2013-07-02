9d78d5dcea09250dc767d216469dcac8fbf7a563
Vinh Vo <vinh.votruong@gmail.com>
2013-07-02 20:57:56 +0700

    user_widget that display a menu for user

---
+++ application/modules/login/views/user_widget.php
@@ -0,0 +1,7 @@
+<code class="user-widget">
+  <?php echo $first_name.' '. $last_name;?> &middot;
+  <?php echo anchor(base_url().'site/members_area','Dashboard');?> |
+  <?php echo anchor(base_url().'site/profile/'.$id,'Profile');?> |
+  <?php echo anchor(base_url().'site/message/'.$id,'Message');?> |
+  <?php echo anchor(base_url().'login/logout', 'Logout');?>
+</code>
