62d119ebd87c2fd8b2d275e6ac7099bf706b4701
Vinh Vo <vinh.votruong@gmail.com>
2013-07-02 21:31:11 +0700

    add user_widget to the page of showing priceboard

--- application/modules/priceboard/views/show_priceboard_view.php
+++ application/modules/priceboard/views/show_priceboard_view.php
@@ -1,6 +1,8 @@
 <?php $this->load->view('includes/header'); ?>
+
+<div id="wrapper">
+<?php echo modules::run('login/cp');?>
 <h2>Lịch sử giá</h2>
-<div id="priceboard">
 <?php echo $main_content;?>
 </div>
 
