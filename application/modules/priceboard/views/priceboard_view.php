dc6f25ed776b43902919fe3e9f99ca597cc897f5
Vinh Vo <vinh.votruong@gmail.com>
2013-07-02 21:10:50 +0700

    priceboard_view

--- application/modules/priceboard/views/priceboard_view.php
+++ application/modules/priceboard/views/priceboard_view.php
@@ -1,15 +1,12 @@
 <?php $this->load->view('includes/header'); ?>
 
 <div id="container">
-	<h1><?php echo($heading);?></h1>
+	<h1>Bảng giá Nhân xô gửi kho</h1>
 
-    <?php echo form_open('#','id=myform');?>
+    <?php echo form_open('priceboard/insert','id=myform');?>
     <?php echo form_input('price');?>
     <?php echo form_submit('submit', 'Submit');?>
     <?php echo form_close();?>
-
-    <?php $this->table->set_heading(array('id', 'date', 'price'));?>
-    <?php echo $this->table->generate($records); ?>
 
 	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds</p>
 </div>
