d166755b7654a4a83734d8c669b2abdc5f0746b6
Vinh Vo <vinh.votruong@gmail.com>
2013-07-02 21:34:56 +0700

    add a link to the page of inserting an entry for the priceboard

--- application/modules/priceboard/views/show_priceboard_view.php
+++ application/modules/priceboard/views/show_priceboard_view.php
@@ -4,6 +4,7 @@
 <?php echo modules::run('login/cp');?>
 <h2>Lịch sử giá</h2>
 <?php echo $main_content;?>
+<?php echo anchor(base_url().'priceboard/insert', "Thêm");?>
 </div>
 
 <?php $this->load->view('includes/tut_info'); ?>
