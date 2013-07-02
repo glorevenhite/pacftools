aff253cd5817b4172ed1394bcb92346a83f82aaa
Vinh Vo <vinh.votruong@gmail.com>
2013-07-02 20:51:38 +0700

    update user_model

--- application/modules/login/models/User_model.php
+++ application/modules/login/models/User_model.php
@@ -12,4 +12,57 @@ class User_model extends CI_Model {
 		}
 
 	}
+
+	function create_member() {
+		$new_member_insert_data = array(
+			'first_name' => $this->input->post('first_name'),
+			'last_name' => $this->input->post('last_name'),
+			'email_address' => $this->input->post('email_address'),
+			'username' => $this->input->post('username'),
+			'password' => md5($this->input->post('password'))
+		);
+
+		$insert = $this->db->insert('users', $new_member_insert_data);
+		return $insert;
+	}
+
+	/*
+	 * The following function call has a default variable $id. This allows us to an option of finding
+	 * a user by ID rather than by username. This made optional by setting it to false in the declaration
+	 */
+	function get_member_details($id = false) {
+	  if(!$id) {
+	    // check whether login credential has been stored in session variables
+        if($this->session->userdata('username')) {
+          // retrieve member details matching with username
+          $this->db->where('username', $this->session->userdata('username'));
+        }
+        else {
+          // Return a non logged in person from accessing member profile dashboard
+          return false;
+        }
+	  }
+	  else {
+	    // get user detail by id
+	    $this->db->where('id', $id);
+	  }
+
+	  // Find all records that match this query
+	  $query = $this->db->get('users');
+	  // We haven't set the unique option for username so we return the last user created with selected username
+	  if($query->num_rows() > 0) {
+	    // get the last row
+        $row = $query->last_row();
+        // Assign the row to our return array
+        $data['id'] = $row->id;
+        $data['first_name'] = $row->first_name;
+        $data['last_name'] = $row->last_name;
+        // Return the user found
+        return $data;
+	  }
+	  else {
+	    // No result found
+	    return false;
+	  }
+	}
 }
