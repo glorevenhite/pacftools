131c5a8b5a0f611025b8a5548f04f943a572ae1d
Vinh Vo <vinh.votruong@gmail.com>
2013-07-02 21:16:01 +0700

    creating a view for displaying priceboard's history

---
+++ application/modules/priceboard/views/show_priceboard_view.php
@@ -0,0 +1,9 @@
+<?php $this->load->view('includes/header'); ?>
+<h2>Lịch sử giá</h2>
+<div id="priceboard">
+<?php echo $main_content;?>
+</div>
+
+<?php $this->load->view('includes/tut_info'); ?>
+
+<?php $this->load->view('includes/footer'); ?>
