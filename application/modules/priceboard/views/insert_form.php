29f601ad4044e22608b1bf11b282bd042ac8b556
Vinh Vo <vinh.votruong@gmail.com>
2013-07-03 06:21:27 +0700

    redirect user to access denied page if unsucessful login

--- application/modules/priceboard/views/insert_form.php
+++ application/modules/priceboard/views/insert_form.php
@@ -1,16 +1,12 @@
 <?php $this->load->view('includes/header'); ?>
 
-<div id="container">
-	<h1>Bảng giá Nhân xô gửi kho</h1>
-
-    <?php echo form_open('priceboard/insert','id=myform');?>
+<div id="wrapper">
+    <?php echo modules::run('login/cp');?>
+	<h2>Cập nhật giá cà phê gửi kho</h2>
+    <?php echo form_open("priceboard/insert","id='myform'");?>
     <?php echo form_input('price');?>
     <?php echo form_submit('submit', 'Submit');?>
     <?php echo form_close();?>
-
-	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds</p>
 </div>
-
-<?php $this->load->view('includes/tut_info'); ?>
 
 <?php $this->load->view('includes/footer'); ?>
