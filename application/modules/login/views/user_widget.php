3d2e58319222e66ea6f81be82fd19d0bb9a64ab2
Vinh Vo <vinh.votruong@gmail.com>
2013-07-02 21:07:20 +0700

    add a link to price board in user_widget

--- application/modules/login/views/user_widget.php
+++ application/modules/login/views/user_widget.php
@@ -3,5 +3,6 @@
   <?php echo anchor(base_url().'site/members_area','Dashboard');?> |
   <?php echo anchor(base_url().'site/profile/'.$id,'Profile');?> |
   <?php echo anchor(base_url().'site/message/'.$id,'Message');?> |
+  <?php echo anchor(base_url().'priceboard','Bảng giá');?>  |
   <?php echo anchor(base_url().'login/logout', 'Logout');?>
 </code>
