e5821806eb808ca5b247a0abf5a895321d6a47fb
Vinh Vo <vinh.votruong@gmail.com>
2013-07-03 21:43:47 +0700

    make jquery available to use

--- application/views/includes/header.php
+++ application/views/includes/header.php
@@ -5,5 +5,9 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 	<title>Sign Up!</title>
 	<link rel="stylesheet" href="<?php echo base_url();?>/css/style.css" type="text/css" media="screen" />
+	<link rel="stylesheet" href="<?php echo base_url();?>/css/pepper-grinder/jquery-ui-1.10.3.min.css">
+
+    <script src="<?php echo base_url();?>/js/jquery-1.9.1.js"></script>
+    <script src="<?php echo base_url();?>/js/jquery-ui-1.10.3.min.js"></script>
 </head>
 <body>
