69954b6f702452e6f2b38d8737e7be7b0e0c0729
Vinh Vo <vinh.votruong@gmail.com>
2013-07-03 20:16:21 +0700

    for refactoring code

--- application/modules/priceboard/controllers/priceboard.php
+++ application/modules/priceboard/controllers/priceboard.php
@@ -1,38 +1,52 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 class Priceboard extends MX_Controller {
 
-	public function index() {
-        modules::run('login/is_logged_in');
-        $this->load->view('insert_form');
-	}
+  public function index()
+  {
+    modules::run('login/is_logged_in');
+    $this->load->view('insert_form');
+  }
 
-	function insert() {
+  /*
+   *Insert a new entry for priceboard
+   */
+  function insert()
+  {
+    $this->load->library('form_validation');
+    $this->form_validation->set_rules('price', 'Price', 'required');
 
-	  $this->load->library('form_validation');
-	  $this->form_validation->set_rules('price', 'Price', 'required');
-
-	    if($this->form_validation->run() == FALSE) {
-	      $data['main_content'] = "failure";
-	      $this->load->view('includes/template',$data);
-	    }
-	    else {
-	      $this->load->model('Priceboard_model');
-	      $this->Priceboard_model->insert();
-	      $data['main_content'] = "show_priceboard_view";
-		  $this->load->view('includes/template', $data);
-	    }
-	}
-
-	function show() {
+    if($this->form_validation->run() == FALSE)
+    {
+      $data['main_content'] = "failure";
+      $this->load->view('includes/template',$data);
+    }
+    else
+    {
       $this->load->model('Priceboard_model');
-	  $this->load->library('table');
-	  $records = $this->Priceboard_model->getAll();
-	  $this->table->set_heading(array('id', 'date', 'price'));
-	  $data['main_content'] = $this->table->generate($records);
+      $this->Priceboard_model->insert();
+      $data['main_content'] = "show_priceboard_view";
+      $this->load->view('includes/template', $data);
+    }
+  }
 
-	  $this->load->view('priceboard_widget', $data);
-	}
+  /*
+   * Show history price
+   */
+  function show()
+  {
+    $this->load->model('Priceboard_model');
+    $this->load->library('table');
+
+    // Retrieving all entries
+    $records = $this->Priceboard_model->getAll();
+    // Setting header for table
+    $this->table->set_heading(array('id', 'date', 'price'));
+    // Format output as table for display to browser
+    $data['main_content'] = $this->table->generate($records);
+    // Call the view corresponded
+    $this->load->view('priceboard_widget', $data);
+  }
 }
 
-/* End of file welcome.php */
-/* Location: ./application/controllers/welcome.php */
+/* End of file priceboard.php */
+/* Location: ./application/modules/priceboard/controllers/priceboard.php */
