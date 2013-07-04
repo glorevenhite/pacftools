818d5095de2a6db39aa42703071c4c9f51f80836
Vinh Vo <vinh.votruong@gmail.com>
2013-07-04 21:04:04 +0700

    add pure.css stylesheet from yui

--- application/views/includes/header.php
+++ application/views/includes/header.php
@@ -4,8 +4,9 @@
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 	<title>Sign Up!</title>
-	<link rel="stylesheet" href="<?php echo base_url();?>/css/style.css" type="text/css" media="screen" />
-	<link rel="stylesheet" href="<?php echo base_url();?>/css/pepper-grinder/jquery-ui-1.10.3.min.css">
+	<link rel="stylesheet" href="<?php echo base_url();?>css/style.css" type="text/css" media="screen" />
+	<link rel="stylesheet" href="<?php echo base_url();?>css/smoothness/jquery-ui-1.10.3.css">
+	<link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.2.0/pure-min.css">
 
     <script src="<?php echo base_url();?>/js/jquery-1.9.1.js"></script>
     <script src="<?php echo base_url();?>/js/jquery-ui-1.10.3.min.js"></script>
