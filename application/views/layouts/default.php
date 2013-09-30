fed7a6c41990493262e6ff8604488fc60adf6718
Vinh Vo <vinh.votruong@gmail.com>
2013-09-30 17:47:12 +0700

    remove display title in page

--- application/views/layouts/default.php
+++ application/views/layouts/default.php
@@ -1,8 +1,6 @@
 <?php $this->load->view('includes/header'); ?>
-
-<div id="wrapper">
-  <?php widget::run('site_navigation');?>
-  <h1><?php echo $template['title'];?></h1>
-  <?php $this->load->view($main_content)?>
-</div>
+  <div id="wrapper">
+    <?php widget::run('site_navigation');?>
+    <?php $this->load->view($main_content)?>
+  </div>
 <?php $this->load->view('includes/footer'); ?>
