8f4fec391d99307b72242dfb358f53473a6f966f
Vinh Vo <vinh.votruong@gmail.com>
2013-09-30 17:50:54 +0700

    modify successed page

--- application/modules/docman/views/success.php
+++ application/modules/docman/views/success.php
@@ -1,2 +1,2 @@
 <h1>Congrats!</h1>
-<p>Your action has been done successfully!</p> <?php echo anchor('#', 'Click Here to Back to Previous Page');?></p>
+<p>Your action has been done successfully! <?php echo anchor('#', 'Click Here to Back to Previous Page');?></p>
