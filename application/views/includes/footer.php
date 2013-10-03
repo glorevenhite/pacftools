b49b54ec262d40e4f714f0c08be62e95c98eb62f
glorevenhite <glorevenhite@gmail.com>
2013-10-03 08:38:55 +0700

    removing javascript of select all text when point to input

--- application/views/includes/footer.php
+++ application/views/includes/footer.php
@@ -1,8 +1,2 @@
-	<script type="text/javascript" charset="utf-8">
-	 /* Whener user click on a input text, namely textbox, all text will be selected */
-		$('input').click(function(){
-			$(this).select();
-		});
-	</script>
 </body>
 </html>
